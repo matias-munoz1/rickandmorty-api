6952de8ece0ec9ecf58edb367179da86f5311c19
Matias <mmunozs1996@gmail.com>
2025-03-24 16:57:45 -0300

    ultimas modificaciones

--- src/app/pages/recently-viewed-page/recently-viewed-page.component.ts
+++ src/app/pages/recently-viewed-page/recently-viewed-page.component.ts
@@ -1,10 +1,10 @@
-
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { Character } from '../../services/rick-and-morty.interface';
 import { clearRecentlyViewed } from '../../store/recently-viewed.actions';
+import { Router, ActivatedRoute } from '@angular/router';
 
 interface AppState {
   recentlyViewed: { characters: Character[] };
@@ -17,7 +17,10 @@ interface AppState {
   template: `
     <div class="recently-viewed-page">
       <h2>Historial de Vistos Recientemente</h2>
-      <button (click)="onClear()">Limpiar Historial</button>
+      <div class="buttons">
+        <button (click)="goBackToApi()">Volver</button>
+        <button (click)="onClear()">Limpiar Historial</button>
+      </div>
       <div *ngIf="recentlyViewed$ | async as characters; else noCharacters">
         <ul>
           <li *ngFor="let character of characters">
@@ -33,14 +36,35 @@ interface AppState {
   `,
   styleUrls: ['./recently-viewed-page.component.scss']
 })
-export class RecentlyViewedPageComponent {
+export class RecentlyViewedPageComponent implements OnInit {
   recentlyViewed$: Observable<Character[]>;
+  mode: string = '';
 
-  constructor(private store: Store<AppState>) {
+  constructor(
+    private store: Store<AppState>,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {
     this.recentlyViewed$ = this.store.select(state => state.recentlyViewed.characters);
+  }
+
+  ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe(params => {
+      this.mode = params['mode'] || '';
+    });
   }
 
   onClear(): void {
     this.store.dispatch(clearRecentlyViewed());
+  }
+
+  goBackToApi(): void {
+    if (this.mode === 'rest') {
+      this.router.navigate(['/rest']);
+    } else if (this.mode === 'graphql') {
+      this.router.navigate(['/graphql']);
+    } else {
+      this.router.navigate(['']);
+    }
   }
 }
